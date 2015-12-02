42109e94fd819b246022f8e4afc29696d6cc9189
Andrew Steinmetz <arsteinm@asu.edu>
2015-12-01 18:55:07 -0800

    Added test package with skeleton classes for unit test cases

---
+++ Checkers/src/test/TestGameEngine.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestGameEngine {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testEval() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCopyBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOpponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTurn() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Checkers/src/test/testCheckerMove.java
@@ -0,0 +1,64 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testCheckerMove {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testNoMovesLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testApplyMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsMoveLegal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsWalkLegal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanCaptureIntArrayArrayInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanCaptureIntArrayArrayIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanWalk() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGenerateMoves() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveComputer() {
+		fail("Not yet implemented");
+	}
+
+}
